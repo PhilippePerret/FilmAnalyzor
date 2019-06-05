94625191446bc079aa2348fe30b1d7bb86eacdad
Philippe Perret <philippe.perret@yahoo.fr>
2019-06-05 14:29:40 +0300

    Fixed #110

--- app/js/common/PFA/PFA-calque.js
+++ app/js/common/PFA/PFA-calque.js
@@ -19,7 +19,7 @@ calque:{
       , DCreate(DIV,{id:'CPFA-huit2', class:'huitieme'})
       , DCreate(DIV,{id:'CPFA-huit3', class:'huitieme'})
       , DCreate(DIV,{id:'CPFA-huit4', class:'huitieme'})
-      
+
       , DCreate(DIV,{id:'CPFA-part-EXPO', class:'part', inner:'EXPOSITION'})
       , DCreate(DIV,{id:'CPFA-part-DEV1', class:'part', inner:'DÉVELOPPEMENT PART 1'})
       , DCreate(DIV,{id:'CPFA-part-DEV2', class:'part', inner:'DÉVELOPPEMENT PART 2'})
@@ -29,14 +29,17 @@ calque:{
     let div = DCreate(DIV, {id:'PFA-CALQUE', append:divs})
     UI.sectionTimeline.append(div)
     this.built = true
+    this.show().hide()
   }
 , show(){
-    this.jqObj.css({top: '0px'})
+    this.jqObj.css({top: '30px'})
     this.shown = true
+    return this // pour le chainage
   }
 , hide(){
-  this.jqObj.css({top: '240px'})
+    this.jqObj.css({top: '240px'})
     this.shown = false
+    return this // pour le chainage
   }
 }
 }
